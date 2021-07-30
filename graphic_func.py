1c88eadea8750c9b9b607cb2e361d57bd0509ef1
LeonPyramid <romain.delpy@hotmail.com>
2021-07-30 17:07:38 +0200

    DoubleDictionnaryExtract
    A general function avoiding code dupllication

--- graphic_func.py
+++ graphic_func.py
@@ -15,22 +15,13 @@ def MessageFreqGraph(data_list,time_res,directory):
     """Create a one line graph with the frequency of messages
 
     Args:
-        data_list (list(dict)):   Extracter from .json files
+        data_list (list(dict)):   Extracted from .json files
         time_res (string): the resolution of the graph samples. Put "day" or "month"
         directory (string): the directory where is stored the .json files and where the graph will
         be stored
     """
-    my_dic = {}
-    for data in data_list:
-        #add all existing dates and increase the number off msg by day (or month)
-        for msg in data["messages"]:
-            date = tmstod(msg["timestamp_ms"]).date();
-            if time_res == "month":
-                date = (date.year , date.month)
-            if date in my_dic:
-                my_dic[date] += 1
-            else:
-                my_dic[date] = 1
+    
+    my_dic = DoubleDictionnaryExtract(data_list,time_res,lambda a : True)[0]
     
     AddEmptyDates(my_dic,time_res)
     
@@ -84,3 +75,35 @@ def AddEmptyDates(dict,time_res):
                 yr = start_date[0] + 1
                 mnth = 1
             start_date = (yr,mnth)
+
+def DoubleDictionnaryExtract(data_list,time_res,condition):
+    """Creates two dictionnary and add messages in one if condition(message) is true, in the oser else
+
+    Args:
+        data_list (list(dict)): the dictionnaries extracted form json
+        time_res (string): the resolution of the graph samples. Put "day" or "month"
+        condition (func(dict)->bool): a test function used to determine in which dictionnary a message must go
+
+    Returns:
+        list(dict): return the list of the two dictionnaries
+    """
+
+    dict_0 = {}
+    dict_1 = {}
+    for data in data_list:
+        #add all existing dates and increase the number off msg by day (or month)
+        for msg in data["messages"]:
+            date = tmstod(msg["timestamp_ms"]).date();
+            if time_res == "month":
+                date = (date.year , date.month)
+            if condition(msg):
+                if date in dict_0:
+                    dict_0[date] += 1
+                else:
+                    dict_0[date] = 1
+            else:
+                if date in dict_1:
+                    dict_1[date] += 1
+                else:
+                    dict_1[date] = 1
+    return [dict_0,dict_1]
