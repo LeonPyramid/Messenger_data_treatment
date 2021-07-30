a63d82f917517db221a9914db5a44b29884ab61f
LeonPyramid <romain.delpy@hotmail.com>
2021-07-30 16:09:37 +0200

    Separated main and file_reader + readme edited + vscode friendly
    the project can now be run under vscode with the correnct installation and addons

--- file_reader.py
+++ file_reader.py
@@ -1,15 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 30 11:23:05 2021
-
-@author: Nemes
-"""
-
-#global var
-
 import json
 import os
-import graphic_func
+
 
 def ExtractAllDataFromFolder(directory):
     """
@@ -30,7 +21,7 @@ def ExtractAllDataFromFolder(directory):
     data_list = []
     while(True):
         try:
-            file = open(os.path.join(curdir,"message_"+str(fileNum)+".json"))
+            file = open(os.path.join(directory,"message_"+str(fileNum)+".json"))
             fileNum += 1
         except FileNotFoundError:
             print(fileNum-1 , " fichiers traités")
@@ -39,18 +30,6 @@ def ExtractAllDataFromFolder(directory):
         data_list.append(data)
         
     return data_list
-
-
-######      MAIN         ######
-#open the fodler message, where all the subfolders are
-
-direc =  os.path.join(os.getcwd(), "messages")
-for folderName in os.listdir(direc):
-    curdir = os.path.join(direc, folderName)
-    if os.path.isdir(curdir):
-        data_list = ExtractAllDataFromFolder(curdir)
-        graphic_func.MessageFreqGraph(data_list,"month", curdir)
-
             
             
 
---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 11:23:05 2021
+
+@author: Romain Delpy & Emile Egreteau-Druet
+"""
+
+
+
+import os
+import graphic_func
+import file_reader
+
+direc =  os.path.join(os.getcwd(), "messages")
+for folderName in os.listdir(direc):
+    curdir = os.path.join(direc, folderName)
+    if os.path.isdir(curdir):
+        data_list = file_reader.ExtractAllDataFromFolder(curdir)
+        graphic_func.MessageFreqGraph(data_list,"month", curdir)
